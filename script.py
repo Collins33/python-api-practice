2607d5ba3a602f1ad5bd2e30954edd71750cd203
Collins njau <collinsnjau39@gmail.com>
2018-01-29 14:56:23 +0300

    I have added the get request using the requests library

--- script.py
+++ script.py
@@ -1,8 +1,16 @@
 from urllib.request import urlopen
+import requests
 
+
+#making a request to the website
 kittens=urlopen("http://placekitten.com/")
 response=kittens.read()
 
 body=response[559:1000]
 
 print(body)
+
+#using the requests library
+r=requests.get("http://www.google.com")
+
+print(r)
