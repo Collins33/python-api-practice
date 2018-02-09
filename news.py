0ea18acbee2bb87f4b8e5d7a55f3de99c6201518
Collins njau <collinsnjau39@gmail.com>
2018-02-09 23:12:07 +0300

    I have put the api key in a separate file

--- news.py
+++ news.py
@@ -5,11 +5,11 @@ from credentials import *
 
 
 
-
+news_url="https://newsapi.org/v2/everything?q=health&sortBy=publishedAt&apiKey=" +str(news_api)
 json_object=urllib2.urlopen(news_url)#using urllib urlopen method to open the url
 
 data= json.load(json_object)#turn the response into json
 
 
 for news in data['articles']:
-    print (news)
+    print (news["description"])
