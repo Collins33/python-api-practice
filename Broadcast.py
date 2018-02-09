ec28f73e56a991f68e4bc16029e6c9e9c28919a4
Collins njau <collinsnjau39@gmail.com>
2018-02-10 00:22:40 +0300

    I have added the broadcast class which is the object to be built by the response

---
+++ Broadcast.py
@@ -0,0 +1,6 @@
+class Broadcast(object):
+    #initialize the class
+    def __init__(self,title,description,url):
+        self.title=title
+        self.description=description
+        self.url=url
--- news.py
+++ news.py
@@ -2,13 +2,14 @@
 import urllib2
 import json
 from credentials import *
+from Broadcast import Broadcast
 
 
 
 
 def getNews(category):
 
-
+    news_list=[]
     news_url="https://newsapi.org/v2/everything?q="+str(category)+"&sortBy=publishedAt&apiKey=" +str(news_api)
     json_object=urllib2.urlopen(news_url)#using urllib urlopen method to open the url
 
@@ -16,8 +17,13 @@ def getNews(category):
 
 
     for news in data['articles']:
+        title=news["title"]
+        description=news["description"]
+        url=news["url"]
+        habari=Broadcast(title,description,url)
+        news_list.append(habari)
+    print(news_list[0].title)    
 
-        print (news["description"])
 
 
 getNews("politics")
